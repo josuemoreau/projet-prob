062ae7558daf0d76474db7b0aa69a9460ac0863e
Josué Moreau <jsmr14.12@gmail.com>
2022-02-13 00:01:27 +0100

    typage de distribution.py

--- distribution.py
+++ distribution.py
@@ -1,101 +1,78 @@
-from typing import List, Tuple, Any, Callable, TypeVar, Optional, Generic, Dict
+from typing import List, Tuple, Callable, TypeVar, Optional, Generic
 from typing_extensions import Protocol
-import scipy as scp
 import scipy.stats as sp
-import scipy.special as scpspec
 import math
 from math import log, comb, inf
 import utils
 import matplotlib.pyplot as plt
-from collections.abc import MutableSequence
+from abc import ABC
 
-A = TypeVar('A')
-B = TypeVar('B', covariant=True)
-
-
-class Sample(Generic[B], Protocol):
-    def __call__(self) -> B:
-        pass
+_A = TypeVar('_A')
+_C = TypeVar('_C')
 
 
-class LogPdf(Generic[B], Protocol):
-    def __call__(self, x):
-        # type : B -> float
-        pass
+class CallableProtocol(Protocol[_C]):
+    __call__: _C
 
 
-class Support(Generic[A]):
-    values: List[A]
+class Multiplicable(ABC):
+    @classmethod
+    def __subclasshook__(cls, C):  # type: ignore
+        if cls is Multiplicable:
+            if any("__mul__" in B.__dict__ for B in C.__mro__):
+                return True
+        return NotImplemented
+
+
+class Support(Generic[_A]):
+    values: List[_A]
     logits: List[float]
     probs: List[float]
 
-    def __init__(self, values, logits, probs):
+    def __init__(self, values: List[_A], logits: List[float],
+                 probs: List[float]):
         self.values = values
         self.logits = logits
         self.probs = probs
-    
-    def __iter__(self):
-        return zip(self.values, self.logits, self.probs)
+
+    def __iter__(self) -> List[Tuple[_A, float, float]]:
+        return list(zip(self.values, self.logits, self.probs))
 
 
-class LazyList(MutableSequence):
-    def __init__(self, f, n):
-        self._eval = [False for i in range(n)]
-        self._list = [None for i in range(n)]
-        self._f = f
-
-    def __len__(self):
-        return len(self._list)
-
-    def __getitem__(self, i):
-        if self._eval[i]:
-            return self._list[i]
-        else:
-            x = self._f(i)
-            self._list[i] = x
-            return x
-
-    def __setitem__(self, i, v):
-        self._list[i] = v
-        self._eval[i] = True
-
-    def __delitem__(self, i):
-        del self._list[i]
-        del self._eval[i]
-
-    def insert(self, i, v):
-        self._list.insert(i, v)
-        self._eval.insert(i, True)
-
-
-class Distrib(Generic[A]):
-    _sample: Sample[A]
-    _logpdf: LogPdf[A]
-    _mean: Optional[Callable[[], float]]
+class Distrib(Generic[_A]):
+    _sample: CallableProtocol[Callable[[], _A]]
+    _logpdf: CallableProtocol[Callable[[_A], float]]
+    _mean: Optional[Callable[[], _A]]
     _var: Optional[Callable[[], float]]
-    _samples: LazyList
-    _support: Optional[Support[A]]
+    _samples: Optional[List[_A]]
+    _support: Optional[Support[_A]]
+    _n: int
 
-    def __init__(self, sample, logpdf, mean=None, var=None, support=None,
-                 n=10000):
-        #samples = [sample() for i in range(n)]
-        #samples = sample(size=n)
-        samples = LazyList(lambda _i: sample(), n)
-
+    def __init__(self, sample: Callable[[], _A], logpdf: Callable[[_A], float],
+                 mean: Optional[Callable[[], _A]] = None,
+                 var: Optional[Callable[[], float]] = None,
+                 support: Optional[Support[_A]] = None,
+                 n: int = 10000):
+        self._n = n
         self._sample = sample
         self._logpdf = logpdf
         self._mean = mean
         self._var = var
-        self._samples = samples
+        self._samples = None
         self._support = support
 
-    def draw(self) -> A:
+    def draw(self) -> _A:
         return self._sample()
 
-    def get_samples(self) -> LazyList:
-        return self._samples
+    def get_samples(self) -> List[_A]:
+        if self._samples is not None:
+            return self._samples
+        else:
+            samples = [self._sample() for i in range(self._n)]
+            self._samples = samples
+            return samples
 
-    def get_support(self, shrink=False) -> Optional[Support[A]]:
+    def get_support(self, shrink: bool = False) -> Optional[Support[_A]]:
         if not shrink:
             return self._support
         else:
@@ -103,9 +80,10 @@ class Distrib(Generic[A]):
             values = self._support.values
             probs = self._support.probs
             values, probs = utils.shrink(values, probs)
-            return Support(values, [math.log(x) if x != 0. else -float('inf') for x in probs], probs)
+            return Support(values, [math.log(x) if x != 0. else -float('inf')
+                                    for x in probs], probs)
 
-    def logpdf(self, x: A) -> float:
+    def logpdf(self, x: _A) -> float:
         return self._logpdf(x)
 
     # def mean_generic(self, transform) -> float:
@@ -116,12 +94,14 @@ class Distrib(Generic[A]):
     #     else:
     #         pass
 
-    def plot(self, plot_with_support=False, plot_style='scatter'):
+    def plot(self, plot_with_support: bool = False,
+             plot_style: str = 'scatter') -> None:
         if plot_with_support:
             if self._support is None:
                 print("Pas de support à plot")
                 return
             supp = self.get_support(shrink=True)
+            assert(isinstance(supp, Support))
             if plot_style == 'bar':
                 plt.bar(supp.values, supp.probs, width=0.05)
             elif plot_style == 'scatter':
@@ -134,7 +114,7 @@ class Distrib(Generic[A]):
                 plt.ylim((-plot_y_size*1/20, plot_y_size*21/20))
             else:
                 print("L'argument plot_style est invalide. Il doit être "\
-                    "'bar', 'scatter', ou 'line'")
+                      "'bar', 'scatter', ou 'line'")
                 return
         else:
             plt.hist(self.get_samples(), 100)
@@ -143,7 +123,7 @@ class Distrib(Generic[A]):
         plt.show()
 
 
-def bernoulli(p):
+def bernoulli(p: float) -> Distrib[int]:
     assert(0 <= p <= 1)
     sample  = lambda: sp.bernoulli.rvs(p)
     logpdf  = lambda x: sp.bernoulli.logpmf(x, p)
@@ -158,7 +138,8 @@ def bernoulli(p):
     support = Support([0, 1], logits, [1.-p, p])
     return Distrib(sample, logpdf, mean, var, support)
 
-def binomial(p, n):
+
+def binomial(p: float, n: int) -> Distrib[int]:
     assert(0 <= p <= 1 and 0 <= n)
     sample  = lambda: sp.binom.rvs(n, p)
     logpdf  = lambda x: sp.binom.logpmf(x, n, p)
@@ -174,31 +155,38 @@ def binomial(p, n):
         support = None
     return Distrib(sample, logpdf, mean, var, support)
 
-def dirac(v):
+
+def dirac(v: _A) -> Distrib[_A]:
     sample = lambda: v
     logpdf = lambda x: 0. if x == v else -inf
     mean   = lambda: v
     var    = lambda: 0.
     return Distrib(sample, logpdf, mean, var)
 
-def support(values, logits):
+
+def support(values: List[_A], logits: List[float]) -> Distrib[_A]:
     assert(len(values) == len(logits))
     probs = utils.normalize(logits)
     sp_distrib = sp.rv_discrete(values=(range(len(values)), probs))
     sample  = lambda: values[sp_distrib.rvs()]
     logpdf  = lambda x: utils.findprob(values, probs, x)
-    _mean = sum(values[i]*probs[i] for i in range(len(values)))
-    _var = sum((values[i] - _mean)**2 for i in range(len(values))) / len(probs)
+    if isinstance(values, Multiplicable):
+        _mean = sum(values[i] * probs[i] for i in range(len(values)))  # type: ignore
+        _var = sum((values[i] - _mean)**2 for i in range(len(values))) / len(probs)  # type: ignore
+    else:
+        raise NotImplemented
     mean    = lambda: _mean
     var     = lambda: _var
     support = Support(values, logits, probs)
-    return Distrib(sample, logpdf, mean, var, support)
+    return Distrib(sample, logpdf, mean, var, support)  # type: ignore
 
-def uniform_support(values):
-    logits = [0]*len(values)
+
+def uniform_support(values: List[_A]) -> Distrib[_A]:
+    logits = [0.]*len(values)
     return support(values, logits)
 
-def beta(a, b):
+
+def beta(a: float, b: float) -> Distrib[float]:
     assert(a > 0. and b > 0.)
     sample  = lambda: sp.beta.rvs(a, b)
     logpdf  = lambda x: sp.beta.logpdf(x, a, b)
@@ -206,7 +194,8 @@ def beta(a, b):
     var     = lambda: sp.beta.var(a, b)
     return Distrib(sample, logpdf, mean, var)
 
-def gaussian(mu, sigma):
+
+def gaussian(mu: float, sigma: float) -> Distrib[float]:
     assert(0. < sigma)
     sample  = lambda: sp.norm.rvs(loc=mu, scale=sigma)
     logpdf  = lambda x: sp.norm.logpdf(x, loc=mu, scale=sigma)
@@ -214,7 +203,8 @@ def gaussian(mu, sigma):
     var     = lambda: sp.norm.var(loc=mu, scale=sigma)
     return Distrib(sample, logpdf, mean, var)
 
-def uniform(a, b):
+
+def uniform(a: float, b: float) -> Distrib[float]:
     assert(a <= b)
     #scipy.stats.uniform(loc=0, scale=1) tire selon une loi uniforme
     #dans l'intervalle [loc, loc+scale]
