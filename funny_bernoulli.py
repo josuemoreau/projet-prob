75f194dffcaae4c343c0102af49a497fdb93bb46
Josué Moreau <jsmr14.12@gmail.com>
2022-02-14 22:45:41 +0100

    maj exemple funny bernoulli avec Metropolis-Hastings

--- funny_bernoulli.py
+++ funny_bernoulli.py
@@ -17,3 +17,4 @@ if __name__ == '__main__':
     rejsamp_test(foo, data, name, plot_with_support, plot_style)
     impsamp_test(foo, data, name, plot_with_support, plot_style)
     enumsamp_test(foo, data, name, plot_with_support, plot_style)
+    metropolis_hastings_test(foo, data, name, 0, plot_with_support, plot_style)
