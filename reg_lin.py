d8602891f1414dd82fcaa41c401c05f937950505
Josué Moreau <jsmr14.12@gmail.com>
2022-02-19 03:29:12 +0100

    typage reg_lin.py

--- reg_lin.py
+++ reg_lin.py
@@ -2,22 +2,31 @@ from inference import ImportanceSampling, MetropolisHastings, Prob, InferenceMet
 from distribution import uniform, gaussian
 import numpy as np
 from matplotlib import pyplot as plt
-from typing import Dict, TypeVar, Type, Optional, Callable, Tuple
+from typing import Dict, TypeVar, Type, Optional, Callable, Tuple, NamedTuple, List
 
-A = TypeVar('A')
-B = TypeVar('B')
+Data = NamedTuple('Data', [
+    ('N', int),
+    ('lower', float),
+    ('upper', float),
+    ('x', List[float]),
+    ('y', List[float]),
+    ('a_lower', float),
+    ('a_upper', float),
+    ('b_lower', float),
+    ('b_upper', float)
+])
 
-def linear_regression(prob: Prob, data) -> Tuple[float, float, float]:
-    a = prob.sample(gaussian(data['a_lower'], data['a_upper']))
-    b = prob.sample(uniform(data['b_lower'], data['b_upper']))
+def linear_regression(prob: Prob, data: Data) -> Tuple[float, float, float]:
+    a = prob.sample(gaussian(data.a_lower, data.a_upper))
+    b = prob.sample(uniform(data.b_lower, data.b_upper))
     s = prob.sample(uniform(0, 1))
-    for i in range(data['N']):
-        prob.observe(gaussian(a * data['x'][i] + b, s), data['y'][i])
+    for i in range(data.N):
+        prob.observe(gaussian(a * data.x[i] + b, s), data.y[i])
     return a, b, s
 
 
-def test_linear_regression(model: Callable[[Prob, A], B], data,\
-     method: Type[InferenceMethod[A, B]], model_name: str, n: int = 1000,
+def test_linear_regression(model: Callable[[Prob, Data], Tuple[float, float, float]], data: Data, \
+     method: Type[InferenceMethod[Data, Tuple[float, float, float]]], model_name: str, n: int = 1000,
      only_best_values: Optional[int]=None, plot_only_mean: bool=True) -> None:
 
     print("-- {}, {} --".format(model_name, method.name()))
@@ -25,30 +34,31 @@ def test_linear_regression(model: Callable[[Prob, A], B], data,\
     dist = m.infer(n=n)
     supp = dist.get_support()
 
+    assert (supp is not None)
     res = [((supp.values[i][0], supp.values[i][1]), supp.probs[i]) for i in range(len(supp.values))]
     sorted_res = sorted(res, key=lambda x: x[1], reverse=True)
 
     if not plot_only_mean:
         for i in range(n if only_best_values is None else only_best_values):
-            x = np.linspace(data['lower'], data['upper'], 2)
+            x = np.linspace(data.lower, data.upper, 2)
             a, b = sorted_res[i][0]
             plt.plot(x, a * x + b, color='blue', alpha=0.1, zorder=0)
 
     if only_best_values is not None:
-        a = np.average([r[0][0] for r in sorted_res[:only_best_values]], 
-                    weights=[r[1] for r in sorted_res[:only_best_values]])
-        b = np.average([r[0][1] for r in sorted_res[:only_best_values]],
-                    weights=[r[1] for r in sorted_res[:only_best_values]])
+        a = float(np.average([r[0][0] for r in sorted_res[:only_best_values]], 
+                             weights=[r[1] for r in sorted_res[:only_best_values]]))
+        b = float(np.average([r[0][1] for r in sorted_res[:only_best_values]],
+                             weights=[r[1] for r in sorted_res[:only_best_values]]))
     else:
-        a = np.average([r[0][0] for r in sorted_res], 
-                    weights=[r[1] for r in sorted_res])
-        b = np.average([r[0][1] for r in sorted_res],
-                    weights=[r[1] for r in sorted_res])
+        a = float(np.average([r[0][0] for r in sorted_res], 
+                               weights=[r[1] for r in sorted_res]))
+        b = float(np.average([r[0][1] for r in sorted_res],
+                             weights=[r[1] for r in sorted_res]))
 
-    x = np.linspace(data['lower'], data['upper'], 2)
+    x = np.linspace(data.lower, data.upper, 2)
     plt.plot(x, a * x + b, color='green', alpha=1, zorder=1)
     
-    plt.scatter(data['x'], data['y'], color='red', zorder=1)
+    plt.scatter(data.x, data.y, color='red', zorder=1)
     plt.title(f"{model_name} - {method.name()}")
     plt.show()
 
@@ -65,17 +75,17 @@ if __name__ == '__main__':
     x_obs = np.linspace(lower, upper, N)
     y_obs = 2 * np.tanh(4 * (x_obs - upper) / upper) + noise * np.random.randn(N)
 
-    data = {
-        'N': N,
-        'lower': lower,
-        'upper': upper,
-        'x': x_obs,
-        'y': y_obs,
-        'a_lower': 0,
-        'a_upper': 1,
-        'b_lower': -10,
-        'b_upper': 10
-    }
+    data = Data(
+        N=N,
+        lower=lower,
+        upper=upper,
+        x=list(x_obs),
+        y=y_obs,
+        a_lower=0,
+        a_upper=1,
+        b_lower=-10,
+        b_upper=10
+    )
     
     test_linear_regression(model, data, ImportanceSampling, name, 2000, 100, True)
     test_linear_regression(model, data, MetropolisHastings, name, 2000, 100, True)
